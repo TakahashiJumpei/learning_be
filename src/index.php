2d69c9592685003ed6d19f3a9c01868a703de680
TakahashiJumpei <b1014098@gmail.com>
2021-08-12 02:11:05 +0900

    トピック作成画面の実装

--- src/index.php
+++ src/index.php
@@ -31,45 +31,9 @@ try {
 
     route($rpath, $method);
 
-?>
-
-    <main class="container py-3">
-        <h1 class="sr-only">ログイン</h1>
-        <div class="my-5">
-            <div class="text-center mb-4">
-                <img width="65" src="images/logo.svg" alt="みんなのアンケート　サイトロゴ">
-            </div>
-            <div class="login-form bg-white p-4 shadow-sm mx-auto rounded">
-                <form action="">
-                    <div class="form-group">
-                        <label for="">ユーザーID</label>
-                        <input type="text" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="">パスワード</label>
-                        <input type="password" class="form-control">
-                    </div>
-                    <div class="d-flex align-items-center justify-content-between">
-                        <div>
-                            <a href="">アカウント登録</a>
-                        </div>
-                        <div>
-                            <input type="submit" value="ログイン" class="btn btn-primary shadow-sm">
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-
-
-    </main>
-    <footer class="text-center p-3">
-        <span class="text-muted"> &copy; CodeMafia</span>
-    </footer>
-
-<?php
 
     require_once SOURCE_BASE . 'partials/footer.php';
+    
 } catch (Throwable $e) {
 
     die('<h1>何かが凄くおかしいようです。</h1>');
