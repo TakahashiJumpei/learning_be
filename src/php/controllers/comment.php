9473a3909ec2a981512e97a7e2c074edf6671070
TakahashiJumpei <b1014098@gmail.com>
2021-08-12 02:49:42 +0900

    コメントリストの作成

---
+++ src/php/controllers/comment.php
@@ -0,0 +1,9 @@
+<?php 
+
+namespace controller\comment;
+
+function get() {
+    require_once SOURCE_BASE . 'views/comment.php';
+}
+
+?>
---
+++ src/php/views/comment.php
@@ -0,0 +1,14 @@
+<main class="container py-3">
+    <ul class="list-unstyled my-5">
+        <li class="bg-white shadow-sm mb-3 rounded p-3">
+            <h2 class="h4 mb-2">
+                <span class="badge badge-success mr-1 align-bottom">賛成</span>
+                <a class="text-body" href="">犬も歩けば棒に当たりますか？</a>
+            </h2>
+            <span>Commented by テストユーザー</span>
+        </li>
+    </ul>
+</main>
+<footer class="text-center p-3">
+    <span class="text-muted"> &copy; CodeMafia</span>
+</footer>
