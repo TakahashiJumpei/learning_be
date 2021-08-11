84e455d81ca3b27579e48d6f03db73ff92a87369
TakahashiJumpei <b1014098@gmail.com>
2021-08-12 03:05:40 +0900

    トピック詳細画面の作成

--- src/php/controllers/comment.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-namespace controller\comment;
-
-function get() {
-    require_once SOURCE_BASE . 'views/comment.php';
-}
-
-?>
---
+++ src/php/controllers/topic_detail.php
@@ -0,0 +1,9 @@
+<?php 
+
+namespace controller\topic_detail;
+
+function get() {
+    require_once SOURCE_BASE . 'views/topic_detail.php';
+}
+
+?>
