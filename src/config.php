316abf576a35a1ced4d15371ad15d4aeed37980f
TakahashiJumpei <b1014098@gmail.com>
2021-08-11 11:17:36 +0900

    confing.phpの正規表現の実装の修正、ヘッダーとフッターをモジュール化

--- src/config.php
+++ src/config.php
@@ -1,12 +1,12 @@
 <?php 
 $uri = $_SERVER['REQUEST_URI'];
-if(preg_match("/(src)/i", $uri, $match)) {
+if(preg_match("/(.+(src))/i", $uri, $match)) {
+    //var_dump($match);
     define('BASE_CONTEXT_PATH', $match[0] . '/');
 }
 
-// if(preg_match("/i", $uri, $match)) {
-//     define('BASE_CONTEXT_PATH', $match[0] . '/');
-// }
+echo BASE_CONTEXT_PATH;
+echo '</br>';
 
 define('BASE_IMAGE_PATH', BASE_CONTEXT_PATH . 'images/');
 define('BASE_JS_PATH', BASE_CONTEXT_PATH . 'js/');
--- src/index.php
+++ src/index.php
@@ -1,6 +1,10 @@
 <?php 
 require_once 'config.php';
+
 echo $_SERVER['REQUEST_URI'];
+
+require_once 'partials/header.php';
+
 if($_SERVER['REQUEST_URI'] === '/app/src/login'){
     require_once 'views/login.php';
 }else if($_SERVER['REQUEST_URI'] === '/app/src/register'){
@@ -8,5 +12,7 @@ if($_SERVER['REQUEST_URI'] === '/app/src/login'){
 }else if($_SERVER['REQUEST_URI'] === '/app/src/'){
     require_once 'views/home.php';
 }
+
+require_once 'partials/footer.php';
 
 ?>
