34da9c7b3f213caaaf637a81351ad50bc3b25c12
TakahashiJumpei <b1014098@gmail.com>
2021-08-12 16:49:37 +0900

    過去の投稿画面の作成

--- src/php/views/home.php
+++ src/php/views/home.php
@@ -1,27 +1,2 @@
 
-    <h1 class="sr-only">ログイン</h1>
-    <div class="my-5">
-        <div class="text-center mb-4">
-            <img width="65" src="images/logo.svg" alt="みんなのアンケート　サイトロゴ">
-        </div>
-        <div class="login-form bg-white p-4 shadow-sm mx-auto rounded">
-            <form action="">
-                <div class="form-group">
-                    <label for="">ユーザーID</label>
-                    <input type="text" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="">パスワード</label>
-                    <input type="password" class="form-control">
-                </div>
-                <div class="d-flex align-items-center justify-content-between">
-                    <div>
-                        <a href="">アカウント登録</a>
-                    </div>
-                    <div>
-                        <input type="submit" value="ログイン" class="btn btn-primary shadow-sm">
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
+    <h1 class="sr-only">トップページ</h1>
