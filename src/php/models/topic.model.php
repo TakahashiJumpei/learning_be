83d5f5e95e55fb41ff0cbdb7263c1367641ff565
TakahashiJumpei <b1014098@gmail.com>
2021-08-13 06:56:08 +0900

    バリデーションの追加

--- src/php/models/topic.model.php
+++ src/php/models/topic.model.php
@@ -1,9 +1,11 @@
-<?php 
+<?php
+
 namespace model;
 
 use lib\Msg;
 
-class TopicModel extends AbstractModel {
+class TopicModel extends AbstractModel
+{
 
     public int $id;
     public string $title;
@@ -12,15 +14,12 @@ class TopicModel extends AbstractModel {
     public int $likes;
     public int $dislikes;
     public string $user_id;
-    public string $nickname;
     public int $del_flg;
-
     protected static $SESSION_NAME = '_topic';
 
-    public function isValidId() {
-
+    public function isValidId()
+    {
         return static::validateId($this->id);
-        
     }
 
     public static function validateId($val) {
@@ -36,8 +35,56 @@ class TopicModel extends AbstractModel {
         return $res;
     }
 
+    public function isValidTitle()
+    {
+        return static::validateTitle($this->title);
+    }
 
+    public static function validateTitle($val) {
+        $res = true;
 
+        if (empty($val)) {
 
+            Msg::push(Msg::ERROR, 'タイトルを入力してください。');
+            $res = false;
+
+        } else {
+
+            if (mb_strlen($val) > 30) {
+
+                Msg::push(Msg::ERROR, 'タイトルは30文字以内で入力してください。');
+                $res = false;
+
+            }
+
+        }
+
+        return $res;
+    }
+
+    public function isValidPublished()
+    {
+        return static::validatePublished($this->published);
+    }
+
+    public static function validatePublished($val) {
+        $res = true;
+
+        if (!isset($val)) {
+
+            Msg::push(Msg::ERROR, '公開するか選択してください。');
+            $res = false;
+
+        } else {
+            // 0、または1以外の時
+            if (!($val == 0 || $val == 1)) {
+
+                Msg::push(Msg::ERROR, '公開ステータスが不正です。');
+                $res = false;
+
+            }
+        }
+
+        return $res;
+    }
 }
-
